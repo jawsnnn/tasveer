35256f3b4f2774cd501d18b67f0924222090e49c
jawsnnn <arpanmalviya@gmail.com>
2021-10-03 13:26:43 -0400

    Added aggdraw

--- src/poc.py
+++ src/poc.py
@@ -3,5 +3,5 @@ from PIL import Image
 width = 400
 height = 300
 
-img = Image.new(mode = "RGB", size = (width, height))
+img = Image.new(mode = "RGB", size = (width, height), color = (255,200,200))
 img.show()
